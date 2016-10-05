b03a50d28432c8be54ca9a72011b893c885c6321
mikewang333 <mikewang.ca@gmail.com>
2016-10-05 00:51:11 -0700

    Finished project

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get '/teachers/new', to: 'teachers#new'
   post '/teachers', to: 'teachers#create'
+  root :to => 'students#new'
+  get '/students/new', to: 'students#new'
+  post '/students', to: 'students#create'
 end
